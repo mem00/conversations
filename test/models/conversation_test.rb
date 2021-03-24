76b5f02f91961db030d264db62f7e334389473dc
Michael McGuire <michael@mcguire.ch>
2021-03-24 10:55:58 -0400

    conversations testing

---
+++ test/models/conversation_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class ConversationTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "should not create conversation without title" do
+    conversation = Conversation.new
+    assert_raise(Exception) { conversation.save }
+  end
+
+  test "should create start_date" do
+    conversation = Conversation.new
+    conversation.title = "Hello World"
+    conversation.save
+    assert conversation.start_date == Date.current
+  end
+end
