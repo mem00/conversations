bd137fe36e585983ac2e54da6e88dba9c8955fc4
Michael McGuire <michael@mcguire.ch>
2021-03-24 18:44:28 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_151613) do
+ActiveRecord::Schema.define(version: 2021_03_24_210445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,5 +28,14 @@ ActiveRecord::Schema.define(version: 2021_03_24_151613) do
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
   end
 
+  create_table "thoughts", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "message_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["message_id"], name: "index_thoughts_on_message_id"
+  end
+
   add_foreign_key "messages", "conversations"
+  add_foreign_key "thoughts", "messages"
 end
